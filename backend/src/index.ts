d9c0282639d416994707fb9a714a0b8184585dae
vishal rai <vishalrai10342@gmail.com>
2025-03-25 02:00:59 +0530

    rm dist

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,5 +24,5 @@ app.use("/api/dashboard", dashboardRoutes);
 
 app.get('/api/dashboard/donar/:id',(req,res)=>{})
 app.get('/api/dashboard/receiver/:id',(req,res)=>{})
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
